34f35940a9dbc4d9215c533a68c7f29cf42e79ca
Aryamann <51781574+AryamannNingombam@users.noreply.github.com>
2020-09-23 13:04:18 +0530

    Improved logic
    Improves the logic of the conversions. Added two helper functions. No need for the user now to uses dashes instead of spaces as that is now handled by the program.

--- encryptMessages.cpp
+++ encryptMessages.cpp
@@ -4,53 +4,15 @@
 
 using namespace std;
 
-//Function to encrypt the given string;
-string encryptThis(string message){
-string encryptedMessage{};
-int counter{1};
- //Boolean value set to decide whether to increment or decrement;
-bool reverse{false};
-for( char i : message){
-    //If counter reaches 10 start incrementing/decrementing the char's ASCII value from 1 again; This number can be increased or decreased
-    //to increase 
-    //or decrease 
-    // the difficulty of decryption;
-    if (counter>5){
-        counter = 1;
-        }
-    //Incrementing/Decrementing the ASCII value of the char ; Increment if reverse is false  || Decrement if reverse is true;
-    encryptedMessage += ((reverse)?char(int(i)-counter):char(int(i)+counter));
-    //Changing the value of reverse to increment or decrement alternatively;
-    reverse = !reverse;
-    //Incrementing counter to increment/decrement one more than the previous character; Would be set back to 1 when counter reaches 10;
-    counter++;
-};
-return encryptedMessage;
-
-}
-
-
-//Function to decrypt the given string;
-string decryptThis(string message){
-    string decryptedMessage{};
-    int counter{1};
-    //Boolean value set to decide whether to increment or decrement; This is set opposite to the value of encryption;
-    bool reverse{true};
-    for( char i : message){
-        
-       //Same logic as before
-    if (counter>5){
-        counter = 1;
-        }
-    decryptedMessage += ((reverse)?char(int(i)-counter):char(int(i)+counter));
-    reverse = !reverse;
-    counter++;
-};
-return decryptedMessage;
-}
 
 
 
+
+//Prototypes; For keeping main code at the top.
+string encryptThis(string);
+string decryptThis(string);
+string convertDashesToSpaces(string);
+string convertSpacesToDashes(string);
 
 int main(){
     string inputFromUser{};
@@ -66,11 +28,7 @@ int main(){
     if (input!=1 && input!=2){cout << "INVALID" << endl;return 0;}
     else {
         bool toEncrypt{input==1};
-        //This is a flaw in the program and using spaces in the sentence cannot lead to a successful decryption; Using '-' or '_' would 
-        //decrypt successfully;
-        cout << "NOTE : Use '-' or '_' instead of spaces to avoid successful decryptions if you're encrypting." << endl;
-        cout << "Use both '-' and '_' to increase the difficulty of decryption from potential threats ." <<endl;
-        cout << "WARNING :DO NOT use signs like '#!\"\"' as this would cause a decryption problem." << endl;
+        cout << "NOTE :DO NOT use signs like '#!\"' as this would cause a decryption problem." << endl;
         cout << "Enter the message to " << ((toEncrypt) ? " encrypt " :  " decrypt ") << " : ";
         cin.ignore();
         getline(cin,inputFromUser);
@@ -85,4 +43,77 @@ int main(){
         cout << "Thanks!" << endl;
     };
     
+}
+
+
+//============================================================
+//ENCRYPTION/DECRYPTION;
+//Function to encrypt the given string;
+string encryptThis(string message){
+message = convertSpacesToDashes(message);
+string encryptedMessage{};
+int counter{1};
+ //Boolean value set to decide whether to increment or decrement;
+bool reverse{false};
+for( char i : message){
+    //If counter reaches 10 start incrementing/decrementing the char's ASCII value from 1 again; This number can be increased or decreased
+    //to increase 
+    //or decrease 
+    // the difficulty of decryption;
+    if (counter>5){
+        counter = 1;
+        };
+    //Incrementing/Decrementing the ASCII value of the char ; Increment if reverse is false  || Decrement if reverse is true;
+    encryptedMessage += ((reverse)?char(int(i)-counter):char(int(i)+counter));
+    //Changing the value of reverse to increment or decrement alternatively;
+    reverse = !reverse;
+    //Incrementing counter to increment/decrement one more than the previous character; Would be set back to 1 when counter reaches 10;
+    counter++;
+};
+return encryptedMessage;
+
+}
+
+
+//Function to decrypt the given string;
+string decryptThis(string message){
+    
+    string decryptedMessage{};
+    int counter{1};
+    //Boolean value set to decide whether to increment or decrement; This is set opposite to the value of encryption;
+    bool reverse{true};
+    for( char i : message){
+        
+       //Same logic as before
+    if (counter>5){
+        counter = 1;
+        }
+    decryptedMessage += ((reverse)?char(int(i)-counter):char(int(i)+counter));
+    reverse = !reverse;
+    counter++;
+};
+
+return convertDashesToSpaces(decryptedMessage);
+}
+
+//====================================================
+
+//CONVERSIONS;
+string convertDashesToSpaces(string s){
+string stringToReturn{};
+
+for (auto i : s){
+    if (i=='-'  || i=='_')stringToReturn+=" ";
+    else stringToReturn+=i;
+};
+return stringToReturn;
+
+}
+string convertSpacesToDashes(string s){
+    string stringToReturn{};
+    for (auto i:s){
+        if (i==' ')stringToReturn+='_';
+        else stringToReturn+=i;
+    };
+    return stringToReturn;
 }
